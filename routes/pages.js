40f0c6c8b18bc93ed5f54d7c1eba025fa54edaa6
Rahul singh <singhrahul8803@gmail.com>
2021-08-31 21:39:28 +0530

    2nd commit

--- routes/pages.js
+++ routes/pages.js
@@ -1,51 +1,46 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 // Get Page model
-var Page = require('../models/page');
+const Page = require("../models/page");
 
 /*
  * GET /
  */
-router.get('/', function (req, res) {
-    
-    Page.findOne({slug: 'home'}, function (err, page) {
-        if (err)
-            console.log(err);
+router.get("/", function (req, res, err) {
+  if (err) {
+    console.log(err);
+  } else {
+    Page.findOne({ slug: "home" }, function (err, page) {
+      if (err) console.log(err);
 
-        res.render('index', {
-            title: page.title,
-            content: page.content
-        });
+      res.render("index", {
+        title: page?.title,
+        content: page?.content,
+      });
     });
-    
+  }
 });
 
 /*
  * GET a page
  */
-router.get('/:slug', function (req, res) {
+router.get("/:slug", function (req, res) {
+  const slug = req.params.slug;
 
-    var slug = req.params.slug;
+  Page.findOne({ slug: slug }, function (err, page) {
+    if (err) console.log(err);
 
-    Page.findOne({slug: slug}, function (err, page) {
-        if (err)
-            console.log(err);
-        
-        if (!page) {
-            res.redirect('/');
-        } else {
-            res.render('index', {
-                title: page.title,
-                content: page.content
-            });
-        }
-    });
-
-    
+    if (!page) {
+      res.redirect("/");
+    } else {
+      res.render("index", {
+        title: page?.title,
+        content: page?.content,
+      });
+    }
+  });
 });
 
 // Exports
 module.exports = router;
-
-
